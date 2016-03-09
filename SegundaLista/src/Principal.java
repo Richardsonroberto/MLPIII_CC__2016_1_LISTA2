5d754f96b3c95993544592dd489f776ed6a2c91f
Richardsonroberto <richardsonroberto@outlook.com>
2016-03-09 20:49:31 -0300

    Added files via upload

---
+++ SegundaLista/src/Principal.java
@@ -0,0 +1,17 @@
+
+public class Principal {
+	
+	public static void main(String[] args){
+		
+		Data d = new Data1();
+		d.dia = 08;
+		d.mes = 88;
+		d.ano = =1;
+		
+		System.out.println(d);
+		
+		
+		
+	}
+
+}
